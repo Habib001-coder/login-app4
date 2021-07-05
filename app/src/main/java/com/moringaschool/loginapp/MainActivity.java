949693e9696b4feea0052f4aa579eff09aa86662
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-05 12:31:37 +0300

    add new changes

--- app/src/main/java/com/moringaschool/loginapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/loginapp/MainActivity.java
@@ -3,12 +3,32 @@ package com.moringaschool.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+ private EditText ename;
+ private EditText epassword;
+ private Button button3;
 
-    @Override
+
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ename = findViewById(R.id.ename);
+        epassword = findViewById(R.id.epassword);
+        button3 = findViewById(R.id.button3);
+
+
+
+        ename.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName =
+            }
+        });
+
     }
-}
+}S
