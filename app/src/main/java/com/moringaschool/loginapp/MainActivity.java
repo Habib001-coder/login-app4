e2d01c9ca6fe05e46add633cf6d1cbc039243399
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-05 14:01:23 +0300

    add the changes made

--- app/src/main/java/com/moringaschool/loginapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/loginapp/MainActivity.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
  private EditText ename;
  private EditText epassword;
  private Button button3;
 
+private String Username = "Admin";
+private String password = "12345678";
 
 @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,18 @@ public class MainActivity extends AppCompatActivity {
         ename.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inputName =
+                String inputName = ename.getText().toString();
+                String inputPassword = epassword.getText().toString();
+                if(inputName.isEmpty() || inputPassword.isEmpty()) {
+
+
+
+                            Toast.makeText(MainActivity.this, "Please enter all the details correctly", Toast.LENGTH_LONG).show();
+                }
+
+
             }
         });
-
+//private boolean validates(String ename,String password){
     }
-}S
+}
