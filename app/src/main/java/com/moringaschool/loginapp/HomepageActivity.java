95b5462ff99b796c10668274db4b31b6d1b75d69
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-06 09:57:15 +0300

    add changes made

--- app/src/main/java/com/moringaschool/loginapp/HomepageActivity.java
+++ app/src/main/java/com/moringaschool/loginapp/HomepageActivity.java
@@ -2,13 +2,33 @@ package com.moringaschool.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomepageActivity extends AppCompatActivity {
+private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        button = findViewById(R.id.ebutton2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ebutton2();
+            }
+
+            private void ebutton2() {
+                Intent intent = new Intent( HomepageActivity.this,CommentsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
