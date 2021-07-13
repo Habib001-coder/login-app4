e2dee250761e07d20b96cf399d5288b76c122952
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-13 23:21:06 +0300

    add the changes made

--- app/src/main/java/com/moringaschool/loginapp/CommentsActivity.java
+++ app/src/main/java/com/moringaschool/loginapp/CommentsActivity.java
@@ -2,13 +2,31 @@ package com.moringaschool.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CommentsActivity extends AppCompatActivity {
+private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+        button = findViewById(R.id.apis);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ebutton2();
+            }
+
+            private void ebutton2() {
+                Intent intent = new Intent( CommentsActivity.this,Constants.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
