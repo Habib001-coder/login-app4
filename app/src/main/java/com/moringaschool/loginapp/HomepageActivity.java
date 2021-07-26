6fb64e72ec1f7b21b7c16b49bdba83519e2d6992
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-26 14:47:38 +0300

    add the changes made

--- app/src/main/java/com/moringaschool/loginapp/HomepageActivity.java
+++ app/src/main/java/com/moringaschool/loginapp/HomepageActivity.java
@@ -8,8 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-//public class HomepageActivity extends AppCompatActivity {
-//private Button button;
+public class HomepageActivity extends AppCompatActivity {
+private Button button;
 
 
 
